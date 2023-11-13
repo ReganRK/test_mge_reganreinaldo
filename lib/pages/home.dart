9c9f08ed6e6afab0b9f8b2d6544435da027e39bf
ReganRK <reganreinal@gmail.com>
2023-11-13 14:10:49 +0700

    HomePage
    Setting navigation bar and components

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:test_mge_reganreinaldo/pages/list.dart';
+import 'package:test_mge_reganreinaldo/pages/profile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,22 +11,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+  List pages = [
+    ListPage(),
+    ProfilePage()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color(0xff5f7bcc),
+        foregroundColor: Colors.white,
         automaticallyImplyLeading: false,
-        title: Text(
+        title: const Text(
           'Welcome, Regan'
         ),
         actions: [
           GestureDetector(
-            child: Image.asset(
-              'assets/images/logout.png'
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+
+            child: SvgPicture.asset(
+              'assets/icons/logout.svg'
             ),
-          )
+          ),
+          const SizedBox(width: 10,)
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        backgroundColor: const Color(0xff5f7bcc),
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(
+              Icons.list,
+              color: Colors.white,
+            ),
+            label: 'list'
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/profile.svg',
+            ),
+            label: 'profile'
+          ),
+        ],
+      ),
+      body: pages[currentIndex],
     );
   }
 }
